d178a47fc576a0f86603d968de7f74ecd9552d6c
Ad Friedman <adien330@yahoo.com>
2019-03-22 17:32:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -94,11 +94,9 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-<<<<<<< HEAD
           :known_for => "GUI",
-=======
           :known_for => "GUI"
->>>>>>> 25c677dd858313ef9ffadc036a5663b79b17b5ab
+
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
@@ -108,11 +106,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-<<<<<<< HEAD
+
  programmer_hash[:alan_kay][:known_for]= alans_new_info
  programmer_hash
-=======
->>>>>>> 25c677dd858313ef9ffadc036a5663b79b17b5ab
 
 end
 
